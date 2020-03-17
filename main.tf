aa662810a17e22960d127f3d8c9ccf7cd463477f
Daiki Horiyama <d19921207@gmail.com>
2020-03-17 21:43:36 +0900

    integrate with ECS infra (from ch10)

--- main.tf
+++ main.tf
@@ -1,3 +1,232 @@
+resource "aws_ecs_cluster" "example" {
+  name = "example"
+}
+
+resource "aws_cloudwatch_log_group" "for_ecs_scheduled_tasks" {
+  name = "/ecs-scheduled-tasks/example"
+  retention_in_days = 180
+}
+
+resource "aws_ecs_task_definition" "example_batch" {
+  family = "example-batch"
+  cpu = "256"
+  memory = "512"
+  network_mode = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  container_definitions = file("./batch_container_definitions.json")
+  execution_role_arn = module.ecs_task_execution_role.iam_role_arn
+}
+
+data "aws_iam_policy" "ecs_task_execution_role_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+data "aws_iam_policy_document" "ecs_task_execution" {
+  source_json = data.aws_iam_policy.ecs_task_execution_role_policy.policy
+
+  statement {
+    effect = "Allow"
+    actions = ["ssm:GetParameters", "kms:Decrypt"]
+    resources = ["*"]
+  }
+}
+
+module "ecs_task_execution_role" {
+  source = "./iam_role"
+  name = "ecs-task-execution"
+  identifier = "ecs-tasks.amazonaws.com"
+  policy = data.aws_iam_policy_document.ecs_task_execution.json
+}
+
+
+resource "aws_cloudwatch_event_rule" "example_batch" {
+  name = "example-batch"
+  description = "important batch process"
+  schedule_expression = "cron(*/2 * * * ? *)"
+}
+
+module "ecs_events_role" {
+  source = "./iam_role"
+  name = "ecs-events"
+  identifier = "events.amazonaws.com"
+  policy = data.aws_iam_policy.ecs_events_role_policy.policy
+}
+
+data "aws_iam_policy" "ecs_events_role_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceEventsRole"
+}
+
+resource "aws_cloudwatch_event_target" "example_batch" {
+  target_id = "example-batch"
+  rule = aws_cloudwatch_event_rule.example_batch.name
+  role_arn = module.ecs_events_role.iam_role_arn
+  arn = aws_ecs_cluster.example.arn
+
+  ecs_target {
+    launch_type = "FARGATE"
+    task_count = 1
+    platform_version = "1.3.0"
+    task_definition_arn = aws_ecs_task_definition.example_batch.arn
+
+    network_configuration {
+      assign_public_ip = "false"
+      subnets = [aws_subnet.private_0.id]
+    }
+  }
+}
+
+
+# ----------------------------------------
+# N/W
+# ----------------------------------------
+
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "example"
+  }
+}
+
+resource "aws_subnet" "public_0" {
+  vpc_id = aws_vpc.example.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "ap-northeast-1a"
+
+  tags = {
+    Name = "example-1a"
+  }
+}
+
+resource "aws_subnet" "public_1" {
+  vpc_id = aws_vpc.example.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "ap-northeast-1c"
+
+  tags = {
+    Name = "example-1c"
+  }
+}
+
+resource "aws_internet_gateway" "example" {
+  vpc_id = aws_vpc.example.id
+
+  tags = {
+    Name = "example"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.example.id
+
+  tags = {
+    Name = "example"
+  }
+}
+
+resource "aws_route" "public" {
+  route_table_id = aws_route_table.public.id
+  gateway_id = aws_internet_gateway.example.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "public_0" {
+  subnet_id = aws_subnet.public_0.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_subnet" "private_0" {
+  vpc_id = aws_vpc.example.id
+  cidr_block = "10.0.65.0/24"
+  availability_zone = "ap-northeast-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "example_private_1a"
+  }
+}
+
+resource "aws_subnet" "private_1" {
+  vpc_id = aws_vpc.example.id
+  cidr_block = "10.0.66.0/24"
+  availability_zone = "ap-northeast-1c"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "example_private_1c"
+  }
+}
+
+resource "aws_route_table" "private_0" {
+  vpc_id = aws_vpc.example.id
+
+  tags = {
+    Name = "example_private_0"
+  }
+}
+
+resource "aws_route_table" "private_1" {
+  vpc_id = aws_vpc.example.id
+
+  tags = {
+    Name = "example_private_1"
+  }
+}
+
+resource "aws_route_table_association" "private_0" {
+  subnet_id = aws_subnet.private_0.id
+  route_table_id = aws_route_table.private_0.id
+}
+
+resource "aws_route_table_association" "private_1" {
+  subnet_id = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private_1.id
+}
+
+resource "aws_eip" "nat_gateway_0" {
+  vpc = true
+  depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_eip" "nat_gateway_1" {
+  vpc = true
+  depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_nat_gateway" "nat_gateway_0" {
+  allocation_id = aws_eip.nat_gateway_0.id
+  subnet_id = aws_subnet.public_0.id
+  depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_nat_gateway" "nat_gateway_1" {
+  allocation_id = aws_eip.nat_gateway_1.id
+  subnet_id = aws_subnet.public_1.id
+  depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_route" "private_0" {
+  route_table_id = aws_route_table.private_0.id
+  nat_gateway_id = aws_nat_gateway.nat_gateway_0.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route" "private_1" {
+  route_table_id = aws_route_table.private_1.id
+  nat_gateway_id = aws_nat_gateway.nat_gateway_1.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+
 resource "aws_ssm_parameter" "db_username" {
   name = "/db/username"
   value = "root"
@@ -9,8 +238,6 @@ resource "aws_ssm_parameter" "db_password" {
   name = "/db/password"
   value = "uninitialized"
   type = "SecureString"
-  description = "データベースのパスワード"
-
   lifecycle {
     ignore_changes = [value]
   }
